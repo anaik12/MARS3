a57e79292bca328207d7ec5c10dd48d83e991c50
anaik12 <ashwinignaik@gmail.com>
2017-03-15 20:03:30 -0500

    after hands and music

--- Assets/LeapMotion/Scripts/LeapHandController.cs
+++ Assets/LeapMotion/Scripts/LeapHandController.cs
@@ -118,21 +118,7 @@ namespace Leap.Unity {
         toBeDeleted.Finish();
       }
     }
-        private bool IsHand(Collider other)
-        {
-            if (other.transform.parent && other.transform.parent.parent && other.transform.parent.parent.GetComponent<HandModel>())
-                return true;
-            else
-                return false;
-        }
 
-        void OnTriggerEnter(Collider other)
-        {
-            if (IsHand(other))
-            {
-                Debug.Log("Yay! A hand collided!");
-            }
-        }
 
         private T requireComponent<T>() where T : Component {
       T component = GetComponent<T>();
